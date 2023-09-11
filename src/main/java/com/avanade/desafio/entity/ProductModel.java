3668965fa49ecd607d03efe27c0f3ae04d2eebe9
adolfobjj <luizadolfo7@hotmail.com>
2023-09-11 09:03:54 -0300

    Classe Entity

--- src/main/java/com/avanade/desafio/entity/ProductModel.java
+++ src/main/java/com/avanade/desafio/entity/ProductModel.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductModel implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
     @Id
