1cc1efedb36aad7eb4264c2ac051cf290ffd4a64
adolfobjj <luizadolfo7@hotmail.com>
2023-09-11 09:01:54 -0300

    Classe Entity

---
+++ src/main/java/com/avanade/desafio/entity/ProductModel.java
@@ -0,0 +1,25 @@
+package com.avanade.desafio.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+}
