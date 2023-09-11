4ba85029a95c1a0f9d19dee7ec9794b25414ff68
adolfobjj <luizadolfo7@hotmail.com>
2023-09-11 13:17:36 -0300

    Correção no productcontroller

---
+++ src/main/java/com/avanade/desafio/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.avanade.desafio.controller;
+
+import com.avanade.desafio.models.ProductModel;
+import com.avanade.desafio.repository.ProductRepository;
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+}
